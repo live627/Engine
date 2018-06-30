d9dc022bb63b0b8dd41fc4d5f36c4e2789255f39
John Rayes <live627@gmail.com>
2018-06-30 15:17:12 -0700

    disable a bunch of windows crap

--- Engine/Engine/systemclass.h
+++ Engine/Engine/systemclass.h
@@ -8,7 +8,50 @@
 ///////////////////////////////
 // PRE-PROCESSING DIRECTIVES //
 ///////////////////////////////
+// target Windows 7 or later
+#define _WIN32_WINNT 0x0601
+
+#include <sdkddkver.h>
+// The following #defines disable a bunch of unused windows stuff.
+// See Windows.h, line 35, for explanation of these directives.
 #define WIN32_LEAN_AND_MEAN
+#define NOGDICAPMASKS
+//#define NOSYSMETRICS
+#define NOMENUS
+//#define NOICONS
+//#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOKERNEL
+#define NONLS
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOMINMAX
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+
+// ID3DX10Font wants typedef TEXTMETRIC definitions et al.
+//#define NOTEXTMETRIC
+#define NOWH
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#define NORPC
+#define NOPROXYSTUB
+#define NOIMAGE
+#define NOTAPE
+
+#define STRICT
 
 
 //////////////
