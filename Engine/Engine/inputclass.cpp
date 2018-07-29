8240b13dbeb660c82c798e39bb41071c12c52a8e
John Rayes <live627@gmail.com>
2018-07-29 13:52:56 -0700

    Remove DirectInput

--- Engine/Engine/inputclass.cpp
+++ Engine/Engine/inputclass.cpp
@@ -13,117 +13,11 @@ InputClass::InputClass(HINSTANCE hinstance, HWND hwnd)
 	// Initialize all the keys to being released and not pressed.
 	for (int i = 0; i<256; i++)
 		m_keys[i] = false;
-
-	//// Initialize the location of the mouse on the screen.
-	//m_mouseX = 0;
-	//m_mouseY = 0;
-	//m_mouseZ = 0;
-}
-
-
-bool InputClass::Initialize()
-{
-	HRESULT result;
-	
-
-	// Initialize the main direct input interface.
-	result = DirectInput8Create(hinstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&m_directInput, NULL);
-	if (FAILED(result))
-	{ 
-		return false;
-	}
-
-	// Initialize the direct input interface for the mouse.
-	result = m_directInput->CreateDevice(GUID_SysMouse, &m_mouse, NULL);
-	if (FAILED(result))
-	{
-		return false;
-	}
-
-	// Set the data format for the mouse using the pre-defined mouse data format.
-	result = m_mouse->SetDataFormat(&c_dfDIMouse2);
-	if (FAILED(result))
-	{
-		return false;
-	}
-
-	// Set the cooperative level of the mouse to share with other programs.
-	result = m_mouse->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE);
-	if (FAILED(result))
-	{
-		return false;
-	}
-
-	// Acquire the mouse.
-	result = m_mouse->Acquire();
-	if (FAILED(result))
-	{
-		return false;
-	}
-
-	return true;
-}
-
-
-void InputClass::Shutdown()
-{
-	// Release the mouse.
-	if (m_mouse)
-	{
-		m_mouse->Unacquire();
-		m_mouse->Release();
-		m_mouse = 0;
-	}
-
-	// Release the main interface to direct input.
-	if (m_directInput)
-	{
-		m_directInput->Release();
-		m_directInput = 0;
-	}
 }
 
 
 void InputClass::Frame()
 {
-	HRESULT result;
-
-
-	// Read the mouse device.
-	result = m_mouse->GetDeviceState(sizeof(DIMOUSESTATE2), (LPVOID)&m_mouseState);
-	if (FAILED(result))
-	{
-		// If the mouse lost focus or was not acquired then try to get control back.
-		if ((result == DIERR_INPUTLOST) || (result == DIERR_NOTACQUIRED))
-		{
-			m_mouse->Acquire();
-		}
-		else
-		{
-			// TODO Implement error output
-		}
-	}
-
-	// Update the location of the mouse cursor based on the change of the mouse location during the frame.
-	//m_mouseX = m_mouseState.lX;
-	//m_mouseY = m_mouseState.lY;
-	//m_mouseZ = m_mouseState.lZ;
-	SetDeltaX(m_mouseState.lX);
-	SetDeltaY(m_mouseState.lY);
-	SetDeltaZ(m_mouseState.lZ);
-
-	for (unsigned int i = 0; i < 8; i++)
-		UpdateButtonState(i, m_mouseState.rgbButtons[i]);
-}
-
-
-void InputClass::GetMousePositionDelta(int& mouseX, int& mouseY)
-{
-	//mouseX = m_mouseX;
-	//mouseY = m_mouseY;
-	mouseX = GetDeltaX();
-	mouseY = GetDeltaY();
-}
 
 
 void InputClass::GetMousePositionForDebug(int& mouseX, int& mouseY)
@@ -133,21 +27,6 @@ void InputClass::GetMousePositionForDebug(int& mouseX, int& mouseY)
 	mouseX = cursor.x;
 	mouseY = cursor.y;
 }
-
-
-void InputClass::UpdateButtonState(unsigned int mouseButton, unsigned char di)
-{
-	if (di & 0x80 && !IsMouseButtonDown((MouseButtonID)mouseButton))
-		buttons |= 1 << mouseButton; //turn the bit flag on
-	else if (!(di & 0x80) && IsMouseButtonDown((MouseButtonID)mouseButton))
-		buttons &= ~(1 << mouseButton); //turn the bit flag off
-}
-
-
-bool InputClass::IsMouseButtonDown(MouseButtonID mouseButton)
-{
-	// Return what state the MouseButton is in (pressed/not pressed).
-	return buttons & (1L << mouseButton) == 0;
 }
 
 
--- Engine/Engine/inputclass.h
+++ Engine/Engine/inputclass.h
@@ -5,42 +5,10 @@
 #define _INPUTCLASS_H_
 
 
-///////////////////////////////
-// PRE-PROCESSING DIRECTIVES //
-///////////////////////////////
-#define DIRECTINPUT_VERSION 0x0800
-
-
-/////////////
-// LINKING //
-/////////////
-#pragma comment(lib, "dinput8.lib")
-#pragma comment(lib, "dxguid.lib")
-
-
-//////////////
-// INCLUDES //
-//////////////
-#include <dinput.h>
-
-
 ///////////////////////
 // MY CLASS INCLUDES //
 ///////////////////////
 #include "game.h"
-
-
-//! Button ID for mouse devices
-enum MouseButtonID {
-	MB_Left = 0,
-	MB_Right,
-	MB_Middle,
-	MB_Button3,
-	MB_Button4,
-	MB_Button5,
-	MB_Button6,
-	MB_Button7
-};
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -50,11 +18,9 @@ class InputClass : public GameObject
 {
 public:
 	InputClass(HINSTANCE hinstance, HWND hwnd);
-	bool Initialize();
-	void Shutdown();
+	bool Initialize() { return true; }
 	void Frame();
 
-	void GetMousePositionDelta(int & mouseX, int & mouseY);
 	void GetMousePositionForDebug(int & mouseX, int & mouseY);
 	bool IsMouseButtonDown(MouseButtonID mouseButton);
 
@@ -66,35 +32,7 @@ public:
 private:
 	HINSTANCE hinstance;
 	HWND hwnd;
-	IDirectInput8 * m_directInput;
-	IDirectInputDevice8* m_mouse;
-	DIMOUSESTATE2 m_mouseState;
-
-	//int m_mouseX, m_mouseY, m_mouseZ;
-
-	//! represents all keys - array position indicates virtual key from Windows meessages
 	bool m_keys[256];
-
-	//! represents all buttons - bit position indicates button down
-	unsigned char buttons = 0x0u;
-
-	//! represents delta of all mouse axis - values from Windows messages
-	// leaast significant byte is delta X
-	// next byte to the left is delta y
-	// next byte to the left is delta z
-	// most significant byte is unused
-	unsigned int dword = 0x0u;
-
-public:
-	constexpr unsigned char GetDeltaZ() const { return(dword >> 16u) & 0xFFu; }
-	constexpr unsigned char GetDeltaY() const { return(dword >> 8u) & 0xFFu; }
-	constexpr unsigned char GetDeltaX() const { return dword & 0xFFu; }
-	void SetDeltaZ(unsigned char deltaZ) { dword = (dword & 0xFF00FFFFu) | (deltaZ << 16u); }
-	void SetDeltaY(unsigned char deltaY) { dword = (dword & 0xFFFF00FFu) | (deltaY << 8u); }
-	void SetDeltaX(unsigned char deltaX) { dword = (dword & 0xFFFFFF00u) | deltaX; }
-
-private:
-	void UpdateButtonState(unsigned int mouseButton, unsigned char di);
 };
 
 #endif
