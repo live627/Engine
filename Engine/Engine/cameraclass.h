c32d03c8d9863891edb3e012446a8480e9461b93
John Rayes <live627@gmail.com>
2019-06-26 17:13:24 -0700

    texture picking

--- Engine/Engine/cameraclass.h
+++ Engine/Engine/cameraclass.h
@@ -27,9 +27,11 @@ class CameraClass : public IGameObject
 public:
 	CameraClass(const CameraClass &) = delete;
 	CameraClass & operator=(const CameraClass &) = delete;
-	CameraClass(InputClass * p_Input)
+	CameraClass(InputClass * p_Input, int screenWidth, int screenHeight)
 		:
-		m_Input(p_Input)
+		m_Input(p_Input),
+		m_screenWidth(screenWidth),
+		m_screenHeight(screenHeight)
 	{
 		SetPosition(0.0f, 0.0f, -1.0f);
 		Render();
@@ -55,7 +57,7 @@ public:
 			m_position.y += temp.y;
 		}
 	}
-	bool Render()
+	void Render()
 	{
 		DirectX::XMVECTOR
 			Eye = DirectX::XMVectorSet(m_position.x, m_position.y, -1.0f, 0.0f),
@@ -63,8 +65,6 @@ public:
 			Up = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
 
 		m_viewMatrix = DirectX::XMMatrixLookAtLH(Eye, LookAt, Up);
-
-		return true;
 	}
 
 	void SetPosition(float x, float y, float z) { m_position = DirectX::XMFLOAT3(x, y, z); }
@@ -86,10 +86,72 @@ public:
 	DirectX::XMFLOAT3 GetPosition() const { return m_position; }
 	DirectX::XMFLOAT3 GetRotation() const { return m_rotation; }
 
+	void ResizeBuffers(int screenWidth, int screenHeight,
+		const DirectX::XMMATRIX & worldMatrix,
+		//const DirectX::XMMATRIX & orthoMatrix,
+		const DirectX::XMMATRIX & projectionMatrix)
+	{
+		m_screenWidth = screenWidth;
+		m_screenHeight = screenHeight;
+		this->worldMatrix = worldMatrix;
+		this->projectionMatrix = projectionMatrix;
+	}
+	
+	DirectX::XMFLOAT3 && ToWorldPosition(const DirectX::XMVECTOR & p_position) const
+	{
+		DirectX::XMVECTOR orig = DirectX::XMVector3Unproject(
+			p_position,
+			0,
+			0,
+			m_screenWidth,
+			m_screenHeight,
+			0.1f,
+			1,
+			projectionMatrix,
+			m_viewMatrix,
+			worldMatrix);
+		DirectX::XMFLOAT3 v2F;    //the float where we copy the v2 vector members
+		DirectX::XMStoreFloat3(&v2F, orig);
+	}
+
+	DirectX::XMFLOAT3 && ToWorldPosition(const DirectX::XMFLOAT3 & p_position) const
+	{
+		return ToWorldPosition(DirectX::XMLoadFloat3(&p_position));
+	}
+
+	DirectX::XMFLOAT3 && ToWorldPosition(const POINT & p_position) const
+	{
+		DirectX::XMMATRIX projection = projectionMatrix;
+		DirectX::XMMATRIX view = m_viewMatrix;
+
+		DirectX::XMMATRIX invProjectionView = DirectX::XMMatrixInverse(
+			&DirectX::XMMatrixDeterminant(view *  projection), (view *  projection));
+		//invViewProjection = invView * invProjection;
+
+		float x = (((2.0f * p_position.x) / m_screenWidth) - 1);
+		float y = -(((2.0f * p_position.y) / m_screenHeight) - 1);
+
+		DirectX::XMVECTOR mousePosition = DirectX::XMVectorSet(x, y, 1.0f, 0.0f);
+
+		auto mouseInWorldSpace = DirectX::XMVector3Transform(mousePosition, invProjectionView);
+		DirectX::XMFLOAT3 v2F;    //the float where we copy the v2 vector members
+		DirectX::XMStoreFloat3(&v2F, mouseInWorldSpace);
+
+		return std::move(v2F);
+		//return ToWorldPosition(DirectX::XMVectorSet(p_position.x, p_position.y, 0, 0));
+	}
+
+	DirectX::XMFLOAT3 && ToWorldPosition(int x, int y) const
+	{
+		return ToWorldPosition(DirectX::XMVectorSet(x, y, 0, 0));
+	}
+
 private:
 	DirectX::XMFLOAT3 m_position, m_rotation;
 	DirectX::XMMATRIX m_viewMatrix;
 	InputClass * m_Input;
+	DirectX::XMMATRIX worldMatrix, projectionMatrix;
+	int m_screenWidth, m_screenHeight;
 };
 
 #endif
